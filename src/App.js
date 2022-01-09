fb94d13ec0d43e35cc1a6e2af83b6599f5fa0bd7
Amit <amitsaliap@gmail.com>
2022-01-09 12:47:21 +0530

    lesson 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "harry";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1>hello {name}</h1>
+      <p> paragraph  </p>
     </div>
+
+
+
+    </>
+   
+  
+
   );
 }
 
